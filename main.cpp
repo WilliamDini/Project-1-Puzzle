95d0c90c9fd645000f739c78f7b0f4133fda74a3
jaeceralde <jaeceralde@yahoo.com>
2024-04-22 22:06:19 -0700

    initial commit for interface branch

--- main.cpp
+++ main.cpp
@@ -4,7 +4,63 @@ using namespace std;
 
 int main() {
 
+  int userChoice = 0;
 
+  cout << "Welcome to [TEAM NAME] 8-Puzzle Solver!" << endl << endl;
+
+  // create an instance of Problem class, with the default puzzle
+
+  while (true) {
+    cout << "Type \"1\" to use a default puzzle or \"2\" to enter your own puzzle." << endl;
+    cin >> userChoice;
+
+    if (userChoice == 1) {
+      break;
+    }
+    
+    else if (userChoice == 2) {
+      cout << "Enter your puzzle, use a zero to represent the blank." << endl;
+      // call the problem function that populates the puzzle array according to the user's desires
+      break;
+    }
+  
+    cin.clear();
+    cin.ignore(80, '\n');
+    cout << endl;
+  }
+
+  cout << "A puzzle has been created." << endl << endl;
+
+  userChoice = 0;   // reset the variable for next choice
+
+  while (true) {
+    cout << "Enter your choice of algorithm. Only type the number of the chosen option." << endl;
+    cout << "1. Uniform Cost Search" << endl;
+    cout << "2. A* with the Misplaced Tile heuristic" << endl;
+    cout << "3. A* with the Euclidean Distance heuristic" << endl;
+  
+    cin >> userChoice;
+
+    if (userChoice == 1) {
+      // call uniform cost search
+      break;
+    }
+
+    else if (userChoice == 2) {
+      // call misplaced tile heuristic
+      break;
+    }
+
+    else if (userChoice == 3) {
+      // call euclidean distance heuristic
+      break;
+    }
+  
+    cin.clear();
+    cin.ignore(80, '\n');
+    cout << endl;
+  }
+  
 
   return 0;
 }
